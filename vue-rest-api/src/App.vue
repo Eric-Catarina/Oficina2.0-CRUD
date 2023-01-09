372dc17ba02d283b651d88596bcf694dae2f4a15
Eric-Catarina <2020954200@teiacoltec.org>
2023-01-09 12:45:22 -0300

    feat: Criação do projeto Vue(Front-End)

---
+++ vue-rest-api/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <div class="max-w-7xl mx-auto">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <main class="max-w-7xl mx-auto min-h-screen">
+    <RouterView />
+  </main>
+</template>
