80274f6c76ed3ca45e9ec561b946505b73e1448b
Eric-Catarina <2020954200@teiacoltec.org>
2023-01-14 11:32:48 -0300

    feat: Alteração da página inicial

--- vue-rest-api/src/router/index.js
+++ vue-rest-api/src/router/index.js
@@ -5,9 +5,9 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '',
+      name: 'Index',
+      component: () => import('../views/orcamentos/OrcamentoIndex.vue')
     },
     {
       path: '/orcamento',
